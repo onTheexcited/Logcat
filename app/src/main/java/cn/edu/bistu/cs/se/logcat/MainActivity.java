f6a4f4b2bab3b7367e65104514983ecb32f3498a
onTheexcited <1046042736@qq.com>
2016-09-04 20:26:56 +0800

    First

---
+++ app/src/main/java/cn/edu/bistu/cs/se/logcat/MainActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.bistu.cs.se.logcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+public class MainActivity extends AppCompatActivity {
+ private static final String TAG="MyActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG,"Verbose Message");
+        Log.d(TAG,"Debug Message");
+        Log.i(TAG,"Info Message");
+        Log.w(TAG,"Warning Message");
+        Log.e(TAG,"Error Message");
+    }
+}
